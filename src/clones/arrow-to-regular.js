27dfe030a7a84d1f4b172f3767fcb00ed7404b34
felipelealdefaria <felipelealdefaria@gmail.com>
2021-07-04 20:06:36 -0300

    feat: created clone arrow and regular expression example

---
+++ src/clones/arrow-to-regular.js
@@ -0,0 +1,10 @@
+export const arrowFunction = (value) => {
+  const { type } = value
+  return type
+}
+
+export function regularFunction (value) {
+  // this is a regular function
+  const { type } = value
+  return type
+}
