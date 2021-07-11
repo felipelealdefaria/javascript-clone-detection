226009d899dfb48fd1ce6d518cb41459a8a6b1a2
felipelealdefaria <felipelealdefaria@gmail.com>
2021-07-11 18:44:04 -0300

    feat: added function to generate AST file in clone-two

--- src/clone-detection/analysis/clone-two.js
+++ src/clone-detection/analysis/clone-two.js
@@ -1,7 +1,8 @@
-import * as typeTwo from '../../clones/type-two.js'
 import { cleanning } from '../libs/pre-processing.js'
 import { parseOptmized } from '../libs/parse-to-ast.js'
 import { printResults } from '../libs/print-results.js'
+import * as typeTwo from '../../clones-example/type-two.js'
+import { generateASTFile } from '../libs/generate-ast-file.js'
 
 const controlTree = parseOptmized(`${typeTwo.arrowTwo}`)
 const challengeTree = parseOptmized(`${typeTwo.regularTwo}`)
@@ -9,4 +10,6 @@ const challengeTree = parseOptmized(`${typeTwo.regularTwo}`)
 const controlCleanned = cleanning(controlTree)
 const challengeCleanned = cleanning(challengeTree)
 
+generateASTFile('clone-two/control.sh', controlCleanned)
+generateASTFile('clone-two/challenge.sh', challengeCleanned)
 printResults('RESULT - TYPE 2:', controlCleanned, challengeCleanned);
