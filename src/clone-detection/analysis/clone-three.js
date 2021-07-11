f7799d356412ad3d3ea14629103a3b62404d106b
felipelealdefaria <felipelealdefaria@gmail.com>
2021-07-11 18:44:12 -0300

    feat: added function to generate AST file in clone-three

--- src/clone-detection/analysis/clone-three.js
+++ src/clone-detection/analysis/clone-three.js
@@ -1,7 +1,8 @@
 import { cleanning } from '../libs/pre-processing.js'
 import { parseOptmized } from '../libs/parse-to-ast.js'
 import { printResults } from '../libs/print-results.js'
-import * as typeThree from '../../clones/type-three.js'
+import { generateASTFile } from '../libs/generate-ast-file.js'
+import * as typeThree from '../../clones-example/type-three.js'
 
 const controlTree = parseOptmized(`${typeThree.arrowThree}`)
 const challengeTree = parseOptmized(`${typeThree.regularThree}`)
@@ -9,5 +10,7 @@ const challengeTree = parseOptmized(`${typeThree.regularThree}`)
 const controlCleanned = cleanning(controlTree)
 const challengeCleanned = cleanning(challengeTree)
 
+generateASTFile('clone-three/control.sh', controlCleanned)
+generateASTFile('clone-three/challenge.sh', challengeCleanned)
 printResults('RESULT - TYPE 3:', controlCleanned, challengeCleanned);
 
