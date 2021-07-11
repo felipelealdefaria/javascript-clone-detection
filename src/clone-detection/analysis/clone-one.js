e433cedce5de79c545d86317dfd6f39fe4d7914e
felipelealdefaria <felipelealdefaria@gmail.com>
2021-07-11 18:43:59 -0300

    feat: added function to generate AST file in clone-one

--- src/clone-detection/analysis/clone-one.js
+++ src/clone-detection/analysis/clone-one.js
@@ -1,8 +1,9 @@
 import { cleanning } from '../libs/pre-processing.js'
 import { printResults } from '../libs/print-results.js'
 import { parseOptmized } from '../libs/parse-to-ast.js'
-import { funcOne } from '../../clones/type-one/control.js'
-import * as challenge from '../../clones/type-one/challenge.js'
+import { generateASTFile } from '../libs/generate-ast-file.js'
+import { funcOne } from '../../clones-example/type-one/control.js'
+import * as challenge from '../../clones-example/type-one/challenge.js'
 
 const controlTree = parseOptmized(`${funcOne}`)
 const challengeTree = parseOptmized(`${challenge.funcOne}`)
@@ -10,4 +11,6 @@ const challengeTree = parseOptmized(`${challenge.funcOne}`)
 const controlCleanned = cleanning(controlTree)
 const challengeCleanned = cleanning(challengeTree)
 
+generateASTFile('clone-one/control.sh', controlCleanned)
+generateASTFile('clone-one/challenge.sh', challengeCleanned)
 printResults('RESULT - TYPE 1:', controlCleanned, challengeCleanned)
