3e1d66464293dfd99535d6b252284c0d3fb06252
felipelealdefaria <felipelealdefaria@gmail.com>
2021-08-11 22:58:39 -0300

    chore: result generated v0.5.2

--- __base-functions.js
+++ __base-functions.js
@@ -1,162 +1,13 @@
 
 
-const generateASTFile_879 = (nameFile, ast) => {
-  const astForRead = util.inspect(ast, false, null);
-  if (nameFile.match("/")) {
-    const splitPath = nameFile.split("/");
-    splitPath.pop();
-    const buildPath = splitPath.reduce((last, current) => last + "/" + current);
-    fs.mkdirSync(`src/clone-detection/ast-output/${buildPath}`, {
-      recursive: true
-    }, err => {
-      if (err) throw err;
-    });
+function fibonacciOne_824(n) {
+  for (var fibonacci = [0, 1], i = 1; i < n; i++) {
+    fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
   }
-  fs.writeFile(`src/clone-detection/ast-output/${nameFile}`, `${astForRead}`, err => {
-    if (err) return console.log(err);
-    console.log(`Generated ast file in [src/clone-detection/ast-output/${nameFile}] ✨`);
-  });
-};
-
-const parseOptmized_833 = snippet => {
-  return ast.binaryExpressionReduction(ast.parse(`${snippet}`));
-};
-
-const parse_634 = snippet => {
-  return ast.parse(`${snippet}`);
-};
-
-const isObject_434 = value => {
-  return typeof value === "object" && value !== null && !Array.isArray(value);
-};
-
-const isParams_274 = (type, params) => {
-  return type === "FunctionDeclaration" && Array.isArray(params);
-};
-
-const convertArrowtoRegular_473 = node => {
-  node = {
-    ...node.expression
-  };
-  node?.params?.map(element => element.name = "params_name");
-  node.id = {
-    type: "Identifier",
-    name: "functiondeclaration_name"
-  };
-  if (node?.type === "ArrowFunctionExpression") node.type = "FunctionDeclaration";
-  const {expression, ...convertedNode} = node;
-  return convertedNode;
-};
-
-const normalizeNamesByNode_724 = tree => {
-  ast.walk(tree, (node, parent) => {
-    if (node?.name) node.name = `${parent?.type}_name`.toLowerCase();
-    if (isParams(parent?.type, parent?.params)) {
-      parent.id.name = `${parent?.type}_name`.toLowerCase();
-      parent.params.map(element => element.name = "params_name");
-    }
-  });
-};
-
-const normalizeLiteralValues_859 = tree => {
-  ast.walk(tree, node => {
-    if (node?.type === "Literal") node.value = typeof node.value;
-  });
-};
-
-const deleteAttrsByNode_521 = tree => {
-  ast.walk(tree, node => {
-    if (node?.generator === false) delete node.generator;
-    if (node?.sourceType === "module") delete node.sourceType;
-  });
-};
-
-const organizeAttrsByNode_348 = node => {
-  const arraySort = Object.entries(node).sort((a, b) => {
-    if (a < b) return 1;
-    if (a > b) return -1;
-    return 0;
-  });
-  return Object.fromEntries(arraySort);
-};
-
-const cleanning_886 = tree => {
-  return ast.replace(tree, node => {
-    if (isObject(node?.expression)) node = convertArrowtoRegular(node);
-    deleteAttrsByNode(tree);
-    normalizeNamesByNode(tree);
-    normalizeLiteralValues(tree);
-    return organizeAttrsByNode(node);
-  });
-};
-
-const printResults_828 = (title, astControl, astChallenge) => {
-  console.log(title);
-  console.log("ast-compare: ", compare(astControl, astChallenge));
-  console.log("string-similarity (Dice's): ", stringSimilarity.compareTwoStrings(JSON.stringify(astControl), JSON.stringify(astChallenge)));
-  console.log("string-comparison (Cosine): ", stringComparision.cosine.similarity(JSON.stringify(astControl), JSON.stringify(astChallenge)));
-  console.log("string-comparison (Levenshtein): ", stringComparision.levenshtein.similarity(JSON.stringify(astControl), JSON.stringify(astChallenge)));
-  console.log("string-comparison (Longest Common Subsequence): ", stringComparision.lcs.similarity(JSON.stringify(astControl), JSON.stringify(astChallenge)));
-  console.log("string-comparison (Metric Longest Common Subsequence): ", stringComparision.mlcs.similarity(JSON.stringify(astControl), JSON.stringify(astChallenge)));
-  console.log("---------------------------------------------------------------------------------------------------------");
-};
-
-const isDirectory_91 = (dirPath, file) => fs.statSync(dirPath + "/" + file).isDirectory();
-
-const getAllFiles_907 = (dirPath, arrayOfFiles) => {
-  arrayOfFiles = arrayOfFiles || [];
-  const files = fs.readdirSync(dirPath);
-  files.forEach(file => {
-    if (["__base-functions.js", "__similarity-matrix.js"].includes(file)) return;
-    if (isDirectory(dirPath, file)) arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
-    if (!isDirectory(dirPath, file)) arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
-  });
-  return arrayOfFiles;
-};
-
-const addTreeWithNameHashed_5 = tree => {
-  ast.walk(tree, node => {
-    if (node?.type === "FunctionDeclaration" && node?.id?.name) node.id.name = `${node.id.name}_${Math.floor(Math.random() * 999 + 1)}`;
-  });
-  ast.walk(tree, (node, parent) => {
-    if (node?.type === "ArrowFunctionExpression" && parent?.id?.name) parent.id.name = `${parent.id.name}_${Math.floor(Math.random() * 999 + 1)}`;
-  });
-};
-
-const cloneDetection_466 = (functionsName, functionsArray) => {
-  const arr = [];
-  const parsedArray = functionsArray.map(element => parseOptmized(element));
-  const cleannedTree = parsedArray.map(tree => JSON.stringify(cleanning(tree)));
-  const arrayLength = cleannedTree.length;
-  console.log(`\n✨✨ SÃO ${arrayLength + 1} FUNÇÕES SENDO ANALISADAS AOS PARES. ✨✨\n`);
-  for (let i = 0; i < arrayLength; i++) {
-    const treeControl = cleannedTree.splice(0, 1);
-    let string = "";
-    for (let j = 0; j < i; j++) {
-      string += " 0.000";
-    }
-    string += " 1.000";
-    for (let k = 0; k < cleannedTree.length; k++) {
-      const simi = stringComparision.levenshtein.similarity(JSON.stringify(treeControl), JSON.stringify(cleannedTree[k]));
-      string += ` ${simi.toFixed(3)}`;
-    }
-    console.log([i], string);
-    arr.push(`${functionsName[i]} ${string}`);
-  }
-  fs.writeFileSync(`${process.cwd()}/__similarity-matrix.csv`, "");
-  arr.forEach(el => {
-    const content = fs.readFileSync(`${process.cwd()}/__similarity-matrix.csv`, "utf-8");
-    fs.writeFileSync(`${process.cwd()}/__similarity-matrix.csv`, `${content}\n${el}`);
-  });
-  console.log(`\n⚡⚡ UMA MATRIZ QUADRADA FOI GERADA EM: ${process.cwd()}/__similarity-matrix.csv ⚡⚡\n`);
-};
-
-function fibonacciOne_458(n) {
-  for (var fibonacci = [0, 1], i = 1; i < n; i++) fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
   return fibonacci;
 }
 
-function fibonacciTwo_425() {
+function fibonacciTwo_239() {
   var fib = [];
   fib[0] = 0;
   fib[1] = 1;
@@ -164,19 +15,20 @@ function fibonacciTwo_425() {
     fib[i] = fib[i - 2] + fib[i - 1];
     console.log(fib[i]);
   }
+  return fib;
 }
 
-const arrowFunction_395 = value => {
+const arrowFunction_82 = value => {
   const {type} = value;
   return type;
 };
 
-function regularFunction_174(value) {
+function regularFunction_906(value) {
   const {type} = value;
   return type;
 }
 
-const arrowFour_775 = (op, valueOne, valueTwo) => {
+const arrowFour_418 = (op, valueOne, valueTwo) => {
   if (op === "sub") {
     return valueOne - valueTwo;
   } else {
@@ -184,7 +36,7 @@ const arrowFour_775 = (op, valueOne, valueTwo) => {
   }
 };
 
-function regularFour_930(operation, firstValue, secondValue) {
+function regularFour_158(operation, firstValue, secondValue) {
   switch (operation) {
     case "sub":
       return firstValue - secondValue;
@@ -193,18 +45,18 @@ function regularFour_930(operation, firstValue, secondValue) {
   }
 }
 
-const arrowThree_395 = (state, value) => {
+const arrowThree_437 = (state, value) => {
   if (state) {
     return value * 3.14;
   }
 };
 
-function regularThree_228(state, value) {
+function regularThree_319(state, value) {
   const pi = 3.14;
   if (state) return value * pi;
 }
 
-const arrowTwo_583 = (op, valueOne, valueTwo) => {
+const arrowTwo_346 = (op, valueOne, valueTwo) => {
   if (op === "sum") {
     return valueOne + valueTwo;
   } else {
@@ -212,7 +64,7 @@ const arrowTwo_583 = (op, valueOne, valueTwo) => {
   }
 };
 
-function regularTwo_387(state, vOne, vTwo) {
+function regularTwo_960(state, vOne, vTwo) {
   if (state === "sum") {
     return vOne + vTwo;
   } else {
@@ -220,17 +72,17 @@ function regularTwo_387(state, vOne, vTwo) {
   }
 }
 
-const arrowFunction_533 = value => {
+const arrowFunction_245 = value => {
   const {type} = value;
   return type;
 };
 
-function regularFunction_110(value) {
+function regularFunction_296(value) {
   const {type} = value;
   return type;
 }
 
-const arrowFour_326 = (op, valueOne, valueTwo) => {
+const arrowFour_155 = (op, valueOne, valueTwo) => {
   if (op === "sub") {
     return valueOne - valueTwo;
   } else {
@@ -238,7 +90,7 @@ const arrowFour_326 = (op, valueOne, valueTwo) => {
   }
 };
 
-function regularFour_13(operation, firstValue, secondValue) {
+function regularFour_461(operation, firstValue, secondValue) {
   switch (operation) {
     case "sub":
       return firstValue - secondValue;
@@ -247,26 +99,26 @@ function regularFour_13(operation, firstValue, secondValue) {
   }
 }
 
-function funcOne_891(valueOne, valueTwo) {
+function funcOne_956(valueOne, valueTwo) {
   return valueOne + valueTwo;
 }
 
-const funcOne_195 = (valueOne, valueTwo) => {
+const funcOne_323 = (valueOne, valueTwo) => {
   return valueOne + valueTwo;
 };
 
-const arrowThree_801 = (state, value) => {
+const arrowThree_610 = (state, value) => {
   if (state) {
     return value * 3.14;
   }
 };
 
-function regularThree_164(state, value) {
+function regularThree_86(state, value) {
   const pi = 3.14;
   if (state) return value * pi;
 }
 
-const arrowTwo_853 = (op, valueOne, valueTwo) => {
+const arrowTwo_922 = (op, valueOne, valueTwo) => {
   if (op === "sum") {
     return valueOne + valueTwo;
   } else {
@@ -274,7 +126,7 @@ const arrowTwo_853 = (op, valueOne, valueTwo) => {
   }
 };
 
-function regularTwo_295(state, vOne, vTwo) {
+function regularTwo_577(state, vOne, vTwo) {
   if (state === "sum") {
     return vOne + vTwo;
   } else {
@@ -282,7 +134,7 @@ function regularTwo_295(state, vOne, vTwo) {
   }
 }
 
-function funcaoSemEXPORT_766(state, vOne, vTwo) {
+function funcaoSemEXPORT_622(state, vOne, vTwo) {
   if (state === "sum") {
     return vOne + vTwo;
   } else {
@@ -290,7 +142,7 @@ function funcaoSemEXPORT_766(state, vOne, vTwo) {
   }
 }
 
-const arrowSemEXPORT_459 = (state, vOne, vTwo) => {
+const arrowSemEXPORT_716 = (state, vOne, vTwo) => {
   if (state === "sum") {
     return vOne + vTwo;
   } else {
