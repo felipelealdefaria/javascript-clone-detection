e40ebd7b955ce20bb25e3eb80d0d6b66789c29c9
felipelealdefaria <felipelealdefaria@gmail.com>
2022-02-20 23:42:32 -0300

    up version to 0.6.1

--- src/clone-detection/libs/read-files.js
+++ src/clone-detection/libs/read-files.js
@@ -1,12 +1,13 @@
 import fs from 'fs'
-import path from "path"
+import path from 'path'
 import ast from 'abstract-syntax-tree'
 import { cloneDetection } from './sort-match.js'
 import { parseOptmized } from './parse-to-ast.js'
 
 const arrayFiles = []
-const __dirname = path.resolve();
-const isDirectory = (dirPath, file) => fs.statSync(dirPath + "/" + file).isDirectory()
+const __dirname = path.resolve()
+const isDirectory = (dirPath, file) =>
+  fs.statSync(dirPath + '/' + file).isDirectory()
 
 // const hashCode = s => s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0)
 
@@ -16,51 +17,79 @@ const getAllFiles = (dirPath, arrayOfFiles) => {
 
   files.forEach((file) => {
     if (['__base-functions.js', '__similarity-matrix.js'].includes(file)) return
-    if (isDirectory(dirPath, file)) arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-    if (!isDirectory(dirPath, file)) arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+    if (isDirectory(dirPath, file))
+      arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles)
+    if (!isDirectory(dirPath, file))
+      arrayOfFiles.push(path.join(__dirname, dirPath, '/', file))
   })
 
   return arrayOfFiles
 }
 
 const addTreeWithNameHashed = (tree) => {
-  ast.walk(tree, (node) => { if (node?.type === 'FunctionDeclaration' && node?.id?.name) node.id.name = `${node.id.name}_${Math.floor(Math.random() * 999 + 1)}` })
-  ast.walk(tree, (node, parent) => { if (node?.type === 'ArrowFunctionExpression' && parent?.id?.name) parent.id.name = `${parent.id.name}_${Math.floor(Math.random() * 999 + 1)}` })
+  ast.walk(tree, (node) => {
+    if (node?.type === 'FunctionDeclaration' && node?.id?.name)
+      node.id.name = `${node.id.name}_${Math.floor(Math.random() * 999 + 1)}`
+  })
+  ast.walk(tree, (node, parent) => {
+    if (node?.type === 'ArrowFunctionExpression' && parent?.id?.name)
+      parent.id.name = `${parent.id.name}_${Math.floor(
+        Math.random() * 999 + 1
+      )}`
+  })
 }
 
-getAllFiles(process.argv[2])
-  .map(file => {
-    fs.writeFileSync(`${process.cwd()}/__base-functions.js`, '')
-    if (!((file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))) return
-    console.log('[READING FILE]:', file)
-    
-    const content = fs.readFileSync(`${file}`, 'utf-8')
-    
-    let fileToTree
-    try {
-      fileToTree = parseOptmized(content)
-    } catch (e) {
-      console.log('ERROR: ', e)
-    }
-    
-    fileToTree.body.forEach((tree) => {
-      addTreeWithNameHashed(tree)
-      
-      if (tree?.type === 'FunctionDeclaration') arrayFiles.push(tree)
-      if (tree?.declaration?.type === 'FunctionDeclaration') arrayFiles.push(tree.declaration)
-  
-      if (tree?.declarations && tree?.declarations[0]?.init?.type === 'ArrowFunctionExpression') arrayFiles.push(tree)
-      if (tree?.declaration?.declarations && tree?.declaration?.declarations[0]?.init?.type === 'ArrowFunctionExpression') arrayFiles.push(tree.declaration)
-    })
-  })
+let countFiles = 0
 
-arrayFiles.forEach(tree => {
-  const contentBase = fs.readFileSync(`${process.cwd()}/__base-functions.js`, 'utf-8')
+getAllFiles(process.argv[2]).map((file) => {
+  fs.writeFileSync(`${process.cwd()}/__base-functions.js`, '')
+  if (!(file.indexOf('.') !== 0 && file.slice(-3) === '.js')) return
+  console.log('[READING FILE]:', file)
+
+  countFiles++
+
+  const content = fs.readFileSync(`${file}`, 'utf-8')
+
+  let fileToTree
+  try {
+    fileToTree = parseOptmized(content)
+  } catch (e) {
+    console.log('ERROR: ', e)
+  }
+
+  fileToTree.body.forEach((tree) => {
+    addTreeWithNameHashed(tree)
+
+    if (tree?.type === 'FunctionDeclaration') arrayFiles.push(tree)
+    if (tree?.declaration?.type === 'FunctionDeclaration')
+      arrayFiles.push(tree.declaration)
+
+    if (
+      tree?.declarations &&
+      tree?.declarations[0]?.init?.type === 'ArrowFunctionExpression'
+    )
+      arrayFiles.push(tree)
+    if (
+      tree?.declaration?.declarations &&
+      tree?.declaration?.declarations[0]?.init?.type ===
+        'ArrowFunctionExpression'
+    )
+      arrayFiles.push(tree.declaration)
+  })
+})
+
+console.log(`\n⚡ QUANTIDADE DE ARQUIVOS ANALISADOS: ${countFiles} ⚡\n`)
+
+arrayFiles.forEach((tree) => {
+  const contentBase = fs.readFileSync(
+    `${process.cwd()}/__base-functions.js`,
+    'utf-8'
+  )
   const fileWrite = `${contentBase}\n\n${ast.generate(tree)}`
 
   fs.writeFileSync(`${process.cwd()}/__base-functions.js`, fileWrite)
 })
-  
+
 const file = fs.readFileSync(`${process.cwd()}/__base-functions.js`, 'utf-8')
 const parseFile = parseOptmized(file)
 
@@ -69,12 +98,24 @@ const arrayFunctions = []
 
 parseFile.body.map((el) => {
   if (el?.id?.name) arrayNames.push(el?.id?.name)
-  if (el?.declarations && el?.declarations[0]?.init?.type === 'ArrowFunctionExpression') arrayNames.push(el?.declarations[0]?.id?.name)
+  if (
+    el?.declarations &&
+    el?.declarations[0]?.init?.type === 'ArrowFunctionExpression'
+  )
+    arrayNames.push(el?.declarations[0]?.id?.name)
 
   if (el?.type !== 'VariableDeclaration') return arrayFunctions.push(el)
   return arrayFunctions.push(el.declarations[0].init)
 })
 
-console.log(`\n⚡⚡ ${arrayFunctions.length} FUNÇÕES EXTRAÍDAS FORAM GERADAS EM: ${process.cwd()}/__base-functions.js ⚡⚡`)
+console.log(
+  `\n⚡⚡ ${
+    arrayFunctions.length
+  } FUNÇÕES EXTRAÍDAS FORAM GERADAS EM: ${process.cwd()}/__base-functions.js ⚡⚡`
+)
 
-cloneDetection(arrayNames, arrayFunctions.map(el => ast.generate(el)), process.argv[3])
+cloneDetection(
+  arrayNames,
+  arrayFunctions.map((el) => ast.generate(el)),
+  process.argv[3]
+)
--- src/clone-detection/libs/sort-match.js
+++ src/clone-detection/libs/sort-match.js
@@ -4,67 +4,122 @@ import stringComparision from 'string-comparison'
 import { cleanning } from '../../clone-detection/libs/pre-processing.js'
 import { parseOptmized } from '../../clone-detection/libs/parse-to-ast.js'
 
-export const cloneDetection = (functionsName, functionsArray, similarityIndex = 0.8) => {
+export const cloneDetection = (
+  functionsName,
+  functionsArray,
+  similarityIndex = 0.8
+) => {
   const arr = []
   const cloneInfo = { count: 0, similarity: 0 }
-  const parsedArray = functionsArray.map(element => parseOptmized(element))
-  const cleannedTree = parsedArray.map((tree, i) => cleanning(tree, functionsName[i]))
-  const cleannedCode = cleannedTree.map(tree => ast.generate(tree))
+  const parsedArray = functionsArray.map((element) => parseOptmized(element))
+  const cleannedTree = parsedArray.map((tree, i) =>
+    cleanning(tree, functionsName[i])
+  )
+  const cleannedCode = cleannedTree.map((tree) => ast.generate(tree))
 
   fs.writeFileSync(`${process.cwd()}/__cleanned-functions.js`, '')
-  
-  cleannedCode.forEach(el => {
-    const content = fs.readFileSync(`${process.cwd()}/__cleanned-functions.js`, 'utf-8')
-    fs.writeFileSync(`${process.cwd()}/__cleanned-functions.js`, `${content}\n${el}`)
+
+  cleannedCode.forEach((el) => {
+    const content = fs.readFileSync(
+      `${process.cwd()}/__cleanned-functions.js`,
+      'utf-8'
+    )
+    fs.writeFileSync(
+      `${process.cwd()}/__cleanned-functions.js`,
+      `${content}\n${el}`
+    )
   })
 
-  console.log(`⚡⚡ FUNÇÕES EXTRAÍDAS E NORMALIZADAS FORAM GERADAS EM: ${process.cwd()}/__cleanned-functions.js ⚡⚡\n`)
-  
-  const arrayLength = cleannedCode.length
-  console.log(`\n✨✨ SÃO ${arrayLength} FUNÇÕES SENDO ANALISADAS AOS PARES. ✨✨\n`)
+  console.log(
+    `⚡⚡ FUNÇÕES EXTRAÍDAS E NORMALIZADAS FORAM GERADAS EM: ${process.cwd()}/__cleanned-functions.js ⚡⚡\n`
+  )
 
-  if (fs.existsSync(`${process.cwd()}/__coefficient-by-functions`)) fs.rmdirSync(`${process.cwd()}/__coefficient-by-functions`, { recursive: true })
+  const arrayLength = cleannedCode.length
+  console.log(
+    `\n✨✨ SÃO ${arrayLength} FUNÇÕES SENDO ANALISADAS AOS PARES. ✨✨\n`
+  )
+
+  if (fs.existsSync(`${process.cwd()}/__coefficient-by-functions`))
+    fs.rmdirSync(`${process.cwd()}/__coefficient-by-functions`, {
+      recursive: true,
+    })
   fs.mkdirSync(`${process.cwd()}/__coefficient-by-functions`)
-  
+
   for (let i = 0; i < arrayLength; i++) {
+    const controlNoNormalized = functionsArray.splice(0, 1)
     const treeControl = cleannedCode.splice(0, 1)
 
     let coefficient = ''
-    for (let j = 0; j < i; j++) { coefficient += ' 0.000' }
+    for (let j = 0; j < i; j++) {
+      coefficient += ' 0.000'
+    }
     coefficient += ' 1.000'
 
     for (let k = 0; k < cleannedCode.length; k++) {
-      const simi = stringComparision.levenshtein.similarity(`${treeControl}`, `${cleannedCode[k]}`)
+      const simi = stringComparision.levenshtein.similarity(
+        `${treeControl}`,
+        `${cleannedCode[k]}`
+      )
       coefficient += ` ${simi.toFixed(3)}`
-      
 
       cloneInfo.similarity = similarityIndex
 
       if (simi >= similarityIndex) {
-        if (!fs.existsSync(`${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`)) fs.writeFileSync(`${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`, `// [CONTROL FUNCTION]: ${functionsName[i]}\n${treeControl}\n`)        
-        const content = fs.readFileSync(`${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`, 'utf-8')
-        fs.writeFileSync(`${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`, `${content}\n// [CHALLENGE FUNCTION]: ${functionsName[i + k + 1]} | SIMILARITY: ${simi}\n${cleannedCode[k]}`)
+        if (
+          !fs.existsSync(
+            `${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`
+          )
+        )
+          fs.writeFileSync(
+            `${process.cwd()}/__coefficient-by-functions/${
+              functionsName[i]
+            }.js`,
+            `// [CONTROL FUNCTION]: ${functionsName[i]}\n${controlNoNormalized}\n`
+          )
 
-        cloneInfo.count++;
+        const content = fs.readFileSync(
+          `${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`,
+          'utf-8'
+        )
+
+        fs.writeFileSync(
+          `${process.cwd()}/__coefficient-by-functions/${functionsName[i]}.js`,
+          `${content}\n// [CHALLENGE FUNCTION]: ${
+            functionsName[i + k + 1]
+          } | SIMILARITY: ${simi}\n${functionsArray[k]}\n`
+        )
+
+        cloneInfo.count++
       }
     }
-    
+
     console.log(`[${i}]${coefficient}`)
     arr.push(`${functionsName[i]}${coefficient}`)
   }
-  
+
   fs.writeFileSync(`${process.cwd()}/__similarity-matrix.csv`, '')
-  
-  arr.forEach(el => {
-    const content = fs.readFileSync(`${process.cwd()}/__similarity-matrix.csv`, 'utf-8')
-    fs.writeFileSync(`${process.cwd()}/__similarity-matrix.csv`, `${content}\n${el}`)
+
+  arr.forEach((el) => {
+    const content = fs.readFileSync(
+      `${process.cwd()}/__similarity-matrix.csv`,
+      'utf-8'
+    )
+    fs.writeFileSync(
+      `${process.cwd()}/__similarity-matrix.csv`,
+      `${content}\n${el}`
+    )
   })
 
+  console.log(
+    `\n👾👾 FOI DETECTADO ${cloneInfo.count} CANDIDATOS A CLONE COM ${cloneInfo.similarity} OU MAIS DE SIMILARIDADE.👾👾\n`
+  )
 
-  console.log(`\n👾👾 FOI DETECTADO ${cloneInfo.count} CANDIDATOS A CLONE COM ${cloneInfo.similarity} OU MAIS DE SIMILARIDADE.👾👾\n`)
-
-  console.log(`⚡⚡ CANDIDATOS A CLONE FORAM GERADOS EM: ${process.cwd()}/__coefficient-by-functions ⚡⚡`)
-  console.log(`⚡⚡ UMA MATRIZ QUADRADA FOI GERADA EM: ${process.cwd()}/__similarity-matrix.csv ⚡⚡\n`)
+  console.log(
+    `⚡⚡ CANDIDATOS A CLONE FORAM GERADOS EM: ${process.cwd()}/__coefficient-by-functions ⚡⚡`
+  )
+  console.log(
+    `⚡⚡ UMA MATRIZ QUADRADA FOI GERADA EM: ${process.cwd()}/__similarity-matrix.csv ⚡⚡\n`
+  )
 }
 
 //--> quanto menor a arvore, menor o tempo de processamento (alem de outros metodos de similaridade)
